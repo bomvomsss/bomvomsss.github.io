fc8f9b62e4d10383faa3f9d0723170b18fbd6e20
Kyla Kim <109022128+bomvomsss@users.noreply.github.com>
2024-09-25 17:45:36 +0900

    메인페이지, 스타일

--- mypage/src/App.js
+++ mypage/src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link, useLocation} from 'react-router-dom';
 import Home from './pages/Home.js';
 import Project from './pages/Project.js';
+import { useState, useEffect } from 'react';
 
 function App() {
+  
+  const [position, setPosition] = useState(0);
+
+  // 스크롤 메뉴 바 오퍼시티
+  function onScroll(){
+    setPosition(window.scrollY);
+  }
+  useEffect(()=>{
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    }
+  }, []);
+  
+  //
+  // const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(()=>{
+    console.log(location);
+  },[location])
 
   return (
     <div className="App">
-      <div className="link-bar">
+      <div className="link-bar" style={{opacity:(position-300)/500}}>
         <Link to='/'>HOME</Link>
         <Link to='/project'>PROJECT</Link>
         <Link to='/contact'>CONTACT</Link>
@@ -21,6 +42,11 @@ function App() {
         <Route path='/skills' element={
           <div>
             <Project/>
+          </div>
+        }/>
+        <Route path='/contact' element={
+          <div>
+
           </div>
         }/>
       </Routes>
--- mypage/src/pages/Home.js
+++ mypage/src/pages/Home.js
@@ -1,14 +1,17 @@
+import { SiNotion,SiPhpstorm,SiTypescript,SiSlack,SiFigma,SiRedux,SiBookstack } from "react-icons/si";
 import { DiReact,DiHtml5,DiCss3Full,DiJsBadge,DiJqueryLogo,DiPhotoshop,DiIllustrator,DiBootstrap,DiMarkdown,DiSass,DiGit,DiPhp,DiMysql } from "react-icons/di";
 
 function Home(){
 
   return (
     <div className="wrap home">
-      <div className="top">Front-end Developer</div>
-      <h1 className="main">
-        <p>Ga Yeong</p>
-        <p>Portfolio</p>
-      </h1>
+      <div className="main">
+        <p className="top">Front-end Developer</p>
+        <h1>
+          <p>Ga Yeong</p>
+          <p>Portfolio</p>
+        </h1>
+      </div>
       <div className="about">
         <h2>김가영</h2>
         <b>1998.05.05</b>
@@ -17,6 +20,64 @@ function Home(){
           <br/>
           할수있셔!
         </p>
+      </div>
+      <div className="career">
+        <h2>CAREER</h2>
+        <div className="licenseBox">
+          <p>(주) 챔프스터디 <i>(해커스 교육그룹)</i><span>2022.10 - 2023.12</span></p>
+          <ol>
+            <li>
+              담당 페이지
+              <ul className="flexBox">
+                <li>해커스 홈페이지</li>
+                <li>해커스 일본어</li>
+                <li>해커스 중국어</li>
+                <li>해커스 톡</li>
+                <li>해커스 영스타</li>
+                <li>해커스 한국사</li>
+                <li>해커스 인강</li>
+                <li>해커스 자격증</li>
+                <li>해커스 변호사</li>
+                <li>해커스 로스쿨</li>
+                <li>해커스 프렙</li>
+              </ul>
+              </li>
+            <li>
+              랜딩 페이지 제작 및 유지보수
+              <ul>
+                <li>디자이너와 커뮤니케이션 후 이벤트 랜딩 페이지 제작 및 기존 페이지를 유지보수 했습니다.</li>
+                <li>이벤트 랜딩 페이지를 두 가지 버전으로 제작하여 A/B 테스트를 진행한 뒤 더 적합한 페이지로 수정하였습니다.</li>
+              </ul>
+            </li>
+            <li>
+              랜딩 속도 개선
+              <ul>
+                <li>관리하는 페이지 별 랜딩 속도를 측정하고 개선이 필요한 페이지를 이미지 압축, 불필요한 공백 제거 등의 방법으로 수정하였습니다.</li>
+                <li>Google Sheet에 팀이 맡은 페이지들과 각 페이지의 이벤트 페이지들까지 정리하여 문서화한 뒤 팀원들이 실시간으로 진행 상황을 공유할 수 있게 했습니다.</li>
+              </ul>
+            </li>
+            <li>
+              메인 페이지 리뉴얼
+              <ul>
+                <li>기획자, 디자이너와 커뮤니케이션 하여 해커스 일본어(웹, 모바일) 메인 페이지를 리뉴얼 했습니다.</li>
+                <li>통일되지 않은 레이아웃을 사용하는 페이지들을 모두 통일하고 잘못된 레이아웃을 수정했습니다.</li>
+              </ul>
+            </li>
+            <li>
+              반응형 웹페이지 개선
+              <ul>
+                <li>태블릿으로 인강을 볼 때 레이아웃이 망가지는 것을 개선하기 위해 사이즈에 따라서 스타일과 레이아웃이 알맞게 변경되게 하였습니다.</li>
+              </ul>
+            </li>
+            <li>
+              컴포넌트 개발
+              <ul>
+                <li>바닐라 JS로 팀에서 자주 사용하는 함수들을 정리하여 팀원들과 공유하였습니다.</li>
+                <li>자주 사용하는 Php 함수 또한 함께 정리하여 작업 시간을 단축하였습니다.</li>
+              </ul>
+            </li>
+          </ol>
+        </div>
       </div>
       <div className="skill">
         <h2>SKILLS ABILITY</h2>
@@ -29,24 +90,24 @@ function Home(){
           <span><DiJqueryLogo color="#0769ad"/>JQuery</span>
           <span><DiReact color="#00ffff"/>React</span>
           <span><DiMarkdown/>Markdown</span>
-          <span>Notion</span>
-          <span><DiPhotoshop color="#01a4f6"/>adobe photoshop</span>
-          <span><DiIllustrator color="#f8971a"/>adobe illustrator</span>
-          <span>phpstorm</span>
+          <span><SiNotion/>Notion</span>
+          <span><DiPhotoshop color="#01a4f6"/>Adobe Photoshop</span>
+          <span><DiIllustrator color="#f8971a"/>Adobe Illustrator</span>
+          <span><SiPhpstorm/>PhpStorm</span>
         </div>
         <p>어느정도 사용할 수 있어요</p>
         <div className="normalSkill">
-          <span>typescript</span>
+          <span><SiTypescript color="#3075c1"/>TypeScript</span>
           <span><DiGit color="#f05032"/>Git</span>
-          <span>Slack</span>
-          <span>Figma</span>
+          <span><SiSlack color="#36b1da"/>Slack</span>
+          <span><SiFigma/>Figma</span>
           <span><DiBootstrap color="#7952b3"/>BootStrap</span>
         </div>
         <p>사용해봤어요</p>
         <div className="canSkill">
-          <span><DiPhp color="#4d598f"/>php</span>
-          <span>Eidt Plus</span>
-          <span>Redux</span>
+          <span><DiPhp color="#4d598f"/>Php</span>
+          <span><SiBookstack color="#e6786d"/>Eidt Plus</span>
+          <span><SiRedux color="#764abc"/>Redux</span>
           <span><DiMysql color="#7b9cb5"/>MySQL</span>
         </div>
       </div>
@@ -73,6 +134,16 @@ function Home(){
             <li>2022.06</li>
           </ul>
         </div>
+        <div className="licenseBox">
+          <p>2종 보통 운전면허</p>
+          <ul>
+            <li>경찰청</li>
+            <li>2019.04</li>
+          </ul>
+        </div>
+      </div>
+      <div className="education">
+
       </div>
     </div>
   )
